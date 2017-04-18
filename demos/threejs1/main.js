93cb5cc9abf1d7ae5f226704a68e802a3849966a
Domenicobrz <domenicobrz@live.it>
2017-04-18 23:43:54 +0200

    Fixed quaternion rotation

--- demos/threejs1/main.js
+++ demos/threejs1/main.js
@@ -19,7 +19,7 @@ function pageInit() {
     camera.position.x = 200;
     camera.position.y = 200;
     camera.position.z = 200;
-    window.controls = new THREE.OrbitControls(camera, renderer.domElement);
+    // window.controls = new THREE.OrbitControls(camera, renderer.domElement);
 
     scene = new THREE.Scene();
 
@@ -55,8 +55,8 @@ function pageInit() {
     var light = new THREE.PointLight(0xffffff, 2, 1000);
     light.position.set(100, 100, 100);
     light.castShadow = true;
-    light.shadow.mapSize.width  = 1024;
-    light.shadow.mapSize.height = 1024;
+    light.shadow.mapSize.width  = 512;
+    light.shadow.mapSize.height = 512;
     scene.add(light);
 
     var ambient = new THREE.AmbientLight( 0x222222 ); // soft white light
@@ -66,8 +66,8 @@ function pageInit() {
 
     var obj = {
         t: 0,
-        extension: 1.3,
-        rotation: 0.06
+        extension: 1.1,
+        rotation: 2.2
     };
 
 
@@ -78,7 +78,7 @@ function pageInit() {
                 targets: obj,
                 t: 1,
                 elasticity: 650,
-                duration: 10500,
+                duration: 2500,
                 update: function (anim) {
                     for(var i = 0, l = scene.children.length - 2; i < l; i++) {
                         var t = obj.t;
@@ -91,8 +91,8 @@ function pageInit() {
                         // scene.children[i].rotation.x = t * op[0] * obj.rotation;
                         // scene.children[i].rotation.y = t * op[1] * obj.rotation;
                         // scene.children[i].rotation.z = t * op[2] * obj.rotation;   
-                                             
-                        scene.children[i].setRotationFromAxisAngle(new THREE.Vector3(1,0,0), obj.t * 20 * obj.rotation);
+                        var axis = new THREE.Vector3(op[0], op[1], op[2]).normalize();                     
+                        scene.children[i].setRotationFromAxisAngle(axis, obj.t * obj.rotation);
                     }
                 }
             });
@@ -114,9 +114,8 @@ function pageInit() {
                                                        op[1] + op[1] * t * obj.extension,
                                                        op[2] + op[2] * t * obj.extension);
 
-                        scene.children[i].rotation.x = t * op[0] * obj.rotation;
-                        scene.children[i].rotation.y = t * op[1] * obj.rotation;
-                        scene.children[i].rotation.z = t * op[2] * obj.rotation;
+                        var axis = new THREE.Vector3(op[0], op[1], op[2]).normalize();                     
+                        scene.children[i].setRotationFromAxisAngle(axis, obj.t * obj.rotation);
                     }
                 }
             });
@@ -131,6 +130,6 @@ function animate(now) {
     requestAnimationFrame(animate);
 
     camera.lookAt(scene.position);
-    controls.update();
+    // controls.update();
     renderer.render(scene, camera);
 }
